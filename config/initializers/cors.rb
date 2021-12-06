f40d5b83b522aaf1963b9311d42deb851d6c75ba
LeDjinn <timour.spiridonov@gmail.com>
2021-12-06 11:41:12 +0100

    Cors corrigé 3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,14 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-    end
+require ::File.expand_path('../config/environment',  __FILE__)
+run Rails.application
+
+require 'rack/cors'
+use Rack::Cors do
+
+  # allow all origins in development
+  allow do
+    origins '*'
+    resource '*', 
+        :headers => :any, 
+        :methods => [:get, :post, :delete, :put, :options]
   end
+end
